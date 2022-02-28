d931510d965a5e25a700c6a95ef37b2097efee07
miste <mistersaxar@gmail.com>
2022-02-28 15:41:01 +0500

    tests

---
+++ Tests/Tests/CreatorTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using CardDealer.Creators;
+using CardDealer.Models.Enums;
+using NUnit.Framework;
+
+namespace CardDealer.Tests;
+
+[TestFixture]
+public class CreatorTest
+{
+    private FullDeckCreator creator;
+
+    [SetUp]
+    public void Init()
+    {
+        creator = new FullDeckCreator();
+    }
+    
+    [Test]
+    public void IsNotEmptyDeck()
+    {
+        Assert.IsNotEmpty(creator.CreateCardSequence());
+    }
+
+    [Test]
+    public void IsLengthCorrect()
+    {
+        var cards = creator.CreateCardSequence();
+        Assert.AreEqual(52, cards.Count);
+    }
+
+    [Test]
+    public void AreCardsOfAllSuitsHaveTheSameCount()
+    {
+        var dict = new Dictionary<Enum, int>();
+        foreach (var e in creator.CreateCardSequence())
+        {
+            if (!dict.ContainsKey(e.Rank))
+                dict.Add(e.Rank, 1);
+            else dict[e.Rank]++;
+        }
+
+        foreach (var e in dict)
+        {
+            Assert.AreEqual(4, e.Value);
+        }
+    }
+}
---
+++ Tests/Tests/ShufflerTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using CardDealer.Creators;
+using CardDealer.Models;
+using CardDealer.Shufflers;
+using NUnit.Framework;
+
+namespace CardDealer.Tests;
+
+[TestFixture]
+public class ShufflerTest
+{
+    private List<ICard> cards;
+    private SimpleShuffle shuffler;
+    
+    [SetUp]
+    public void Init()
+    {
+        cards = new FullDeckCreator().CreateCardSequence();
+        shuffler = new SimpleShuffle();
+    }
+
+    [Test]
+    public void IsLengthHasNotChanged()
+    {
+        var deck = new CardDeck("test", cards);
+        shuffler.Shuffle(deck);
+        Assert.AreEqual(52,deck.Cards.Count);
+    }
+    
+    [Test]
+    public void IsShuffleDontHaveSameOutputOnSameDeck()
+    {
+        var deck1 = new CardDeck("test", cards);
+        var cards2 = new FullDeckCreator().CreateCardSequence();
+        var deck2 = new CardDeck("test", cards2);
+        shuffler.Shuffle(deck1);
+        shuffler.Shuffle(deck2);
+        Assert.AreNotEqual(deck1, deck2);
+    }
+}
